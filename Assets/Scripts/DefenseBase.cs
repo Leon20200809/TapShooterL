ccabd2d8069c9a6b73195cf5e2c6923d7bf2126e
Leon20200809 <kindlyhearted666@gmail.com>
2021-02-21 19:46:23 +0900

    手順12終了。拠点追加 エネミー侵入で破棄

---
+++ Assets/Scripts/DefenseBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class DefenseBase : MonoBehaviour
+{
+    [Header("拠点HP")]
+    public int playerHp;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Debug.Log("接触判定；" + col.gameObject.tag);
+
+            //拠点HP減らす
+            playerHp -= 10;
+            Debug.Log("残拠点HP：" + playerHp);
+
+
+            //エネミー破棄
+            Destroy(col.gameObject);
+        }
+
+    }
+}
