4f7c88e626a35a59f2c789167389c7cbb7668a19
Leon20200809 <kindlyhearted666@gmail.com>
2021-02-27 00:03:01 +0900

    発展3終了。 SOよりエネミーデータを取得しフレキシビリティなエネミー生成を追加 処理の読み解き復習要。

---
+++ Assets/Scripts/EnemyDataSO.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "EnemyDataSO", menuName = "Create EnemyDataSO")]
+
+public class EnemyDataSO : ScriptableObject
+{
+    public List<EnemyData> enemyDataList = new List<EnemyData>();
+
+    [Serializable]
+    public class EnemyData
+    {
+        //通し番号
+        public int no;
+
+        //HP
+        public int hp;
+
+        //攻撃力
+        public int power;
+
+        //画像データ
+        public Sprite enemySprite;
+
+        //タイプ
+        public EnemyType enemyType;
+    }
+}
