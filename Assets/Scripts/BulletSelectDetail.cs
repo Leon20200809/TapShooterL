fb5b4e8b36c15a889b04bbc25c9d93df6578cb9d
Leon20200809 <kindlyhearted666@gmail.com>
2021-03-02 23:08:14 +0900

    発展18終了。 弾種選択ボタン自動生成　4つ OnClick中身別途

---
+++ Assets/Scripts/BulletSelectDetail.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BulletSelectDetail : MonoBehaviour
+{
+    [SerializeField]
+    Button btnBulletSelect;
+
+    BulletSelectManager bulletSelectManager;
+
+    public void SetUpBulletSelectDetail(BulletSelectManager bulletSelectManager)
+    {
+        this.bulletSelectManager = bulletSelectManager;
+
+        btnBulletSelect.onClick.AddListener(OnClickBulletSelect);
+    }
+
+    public void OnClickBulletSelect()
+    {
+        Debug.Log("弾選択");
+    }
+
+}
